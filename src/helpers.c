4a918969c8f24a02c08905cba5fc94205e8c4340
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-03-20 00:45:48 +0100

    cleaned up

--- src/helpers.c
+++ src/helpers.c
@@ -3,7 +3,8 @@
 #include "helpers.h"
 
 int lwmqtt_strlen(lwmqtt_string_t str) {
-  if (str.c_string) {
+  // return string length id not null
+  if (str.c_string != NULL) {
     return (int)strlen(str.c_string);
   }
 
@@ -25,44 +26,65 @@ int lwmqtt_strcmp(lwmqtt_string_t *a, char *b) {
   }
 
   // compare memory
-  return strncmp(a->lp_string.data, b, len);
+  return strncmp(a->lp_string.data, b, (size_t)len);
 }
 
 void lwmqtt_write_c_string(unsigned char **pptr, const char *string) {
+  // get length
   int len = (int)strlen(string);
+
+  // write prefix
   lwmqtt_write_int(pptr, len);
+
+  // write string
   memcpy(*pptr, string, len);
+
+  // advance pointer
   *pptr += len;
 }
 
 void lwmqtt_write_string(unsigned char **pptr, lwmqtt_string_t string) {
+  // write length prefixed string if length is given
   if (string.lp_string.len > 0) {
     lwmqtt_write_int(pptr, string.lp_string.len);
     memcpy(*pptr, string.lp_string.data, string.lp_string.len);
     *pptr += string.lp_string.len;
-  } else if (string.c_string) {
-    lwmqtt_write_c_string(pptr, string.c_string);
-  } else {
-    lwmqtt_write_int(pptr, 0);
+    return;
   }
+
+  // write ordinary string if given
+  if (string.c_string != NULL) {
+    lwmqtt_write_c_string(pptr, string.c_string);
+    return;
+  }
+
+  // write zero
+  lwmqtt_write_int(pptr, 0);
 }
 
 int lwmqtt_read_lp_string(lwmqtt_string_t *str, unsigned char **pptr, unsigned char *end_ptr) {
-  int rc = 0;
-
-  // the first two bytes are the length of the string
-  if (end_ptr - (*pptr) > 1) {                   // enough length to read the integer?
-    str->lp_string.len = lwmqtt_read_int(pptr);  // increments pptr to point past length
-    if (&(*pptr)[str->lp_string.len] <= end_ptr) {
-      str->lp_string.data = (char *)*pptr;
-      *pptr += str->lp_string.len;
-      rc = 1;
-    }
+  // check if at lest 2 bytes
+  if (end_ptr - (*pptr) <= 1) {
+    return 0;
   }
 
-  str->c_string = NULL;
+  // read length
+  int len = lwmqtt_read_int(pptr);
 
-  return rc;
+  // check if string end is overflowing the end pointer
+  if (&(*pptr)[len] > end_ptr) {
+    return 0;
+  }
+
+  // set string
+  str->c_string = NULL;
+  str->lp_string.len = len;
+  str->lp_string.data = (char *)*pptr;
+
+  // advance pointer
+  *pptr += str->lp_string.len;
+
+  return 1;
 }
 
 int lwmqtt_read_int(unsigned char **pptr) {
