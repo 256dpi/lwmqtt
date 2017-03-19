c73f2d42380d756f92134251af1a9073fffc4fdd
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-03-20 00:52:04 +0100

    cleaned up docs

--- src/helpers.h
+++ src/helpers.h
@@ -17,71 +17,77 @@ typedef struct {
   }
 
 /**
- * Return the length of the length prefixed string or C string if there is one, otherwise the length delimited string
+ * Returns the length of the string object.
  *
- * @param str the string to return the length of
- * @return the length of the string
+ * @param str - The string to return the length of.
+ * @return The length of the string.
  */
 int lwmqtt_strlen(lwmqtt_string_t str);
 
 /**
- * Compares an MQTTString to a C string
+ * Compares a string object to a c-string.
  *
- * @param a the MQTTString to compare
- * @param b the C string to compare
- * @return boolean - equal or not
+ * @param a The string object to compare.
+ * @param b The c string to compare.
+ * @return Similarity e.g. strcmp().
  */
 int lwmqtt_strcmp(lwmqtt_string_t *a, char *b);
 
 /**
- * ???
+ * Reads a string object from the buffer and populates the passed structure.
  *
- * @param str the length prefixed string structure into which the data is to be read
- * @param pptr pointer to the output buffer - incremented by the number of bytes used & returned
- * @param end_ptr pointer to the end of the data: do not read beyond
- * @return 1 if successful, 0 if not
+ * @param str The structure into which the data is to be read.
+ * @param pptr Pointer to the output buffer - incremented by the number of bytes used & returned.
+ * @param end_ptr Pointer to the end of the data: do not read beyond.
+ * @return One if successful, zero if not.
  */
 int lwmqtt_read_lp_string(lwmqtt_string_t *str, unsigned char **pptr, unsigned char *end_ptr);
 
 /**
- * Writes a "UTF" string to an output buffer.  Converts C string to length-delimited.
+ * Writes a string to an output buffer.
  *
- * @param pptr pointer to the output buffer - incremented by the number of bytes used & returned
- * @param string the C string to write
+ * @param pptr Pointer to the output buffer - incremented by the number of bytes used & returned.
+ * @param The c-string to write.
  */
 void lwmqtt_write_c_string(unsigned char **pptr, const char *string);
 
+/**
+ * Writes a string to an output buffer.
+ *
+ * @param pptr Pointer to the output buffer - incremented by the number of bytes used & returned.
+ * @param The c-string to write.
+ */
 void lwmqtt_write_string(unsigned char **pptr, lwmqtt_string_t string);
 
 /**
- * Calculates an integer from two bytes read from the input buffer
+ * Calculates an integer from two bytes read from the input buffer.
  *
- * @param pptr pointer to the input buffer - incremented by the number of bytes used & returned
- * @return the integer value calculated
+ * @param pptr Pointer to the input buffer - incremented by the number of bytes used & returned.
+ * @return The integer value calculated.
  */
 int lwmqtt_read_int(unsigned char **pptr);
 
 /**
  * Reads one character from the input buffer.
  *
- * @param pptr pointer to the input buffer - incremented by the number of bytes used & returned
- * @return the character read
+ * @param pptr Pointer to the input buffer - incremented by the number of bytes used & returned.
+ * @return The character read.
  */
 unsigned char lwmqtt_read_char(unsigned char **pptr);
 
 /**
  * Writes one character to an output buffer.
  *
- * @param pptr pointer to the output buffer - incremented by the number of bytes used & returned
- * @param chr the character to write
+ * @param pptr Pointer to the output buffer - incremented by the number of bytes used & returned.
+ * @param chr The character to write
  */
 void lwmqtt_write_char(unsigned char **pptr, unsigned char chr);
 
 /**
  * Writes an integer as 2 bytes to an output buffer.
  *
- * @param pptr pointer to the output buffer - incremented by the number of bytes used & returned
- * @param num the integer to write
+ * @param pptr Pointer to the output buffer - incremented by the number of bytes used & returned.
+ * @param num The integer to write.
  */
 void lwmqtt_write_int(unsigned char **pptr, int num);
 
