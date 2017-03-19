a70042e2a0c6f8a8b948db1d3046f381661b5046
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-03-19 17:28:44 +0100

    added second test

--- tests/connect.cpp
+++ tests/connect.cpp
@@ -7,7 +7,7 @@ extern "C" {
 
 #include "macros.h"
 
-TEST(ConnectTest, IsEqual) {
+TEST(ConnectTest, ConnectPacketSerialize1) {
   unsigned char pkt[62] = {
       LWMQTT_CONNECT_PACKET << 4,
       60,
@@ -73,7 +73,7 @@ TEST(ConnectTest, IsEqual) {
       't',
   };
 
-  unsigned char buf[256];
+  unsigned char buf[62];
 
   lwmqtt_will_t will = lwmqtt_default_will;
   will.topic.c_string = (char*)"will";
@@ -87,7 +87,31 @@ TEST(ConnectTest, IsEqual) {
   opts.username.c_string = (char*)"surgemq";
   opts.password.c_string = (char*)"verysecret";
 
-  int l = lwmqtt_serialize_connect(buf, 256, &opts, &will);
+  int l = lwmqtt_serialize_connect(buf, 62, &opts, &will);
+
+  EXPECT_ARRAY_EQ(unsigned char, pkt, buf, l);
+}
+
+TEST(ConnectTest, ConnectPacketSerialize2) {
+  unsigned char pkt[14] = {
+      LWMQTT_CONNECT_PACKET << 4,
+      12,
+      0, // Protocol String MSB
+      4, // Protocol String LSB
+      'M', 'Q', 'T', 'T',
+      4,  // Protocol level 4
+      2,  // Connect Flags
+      0,  // Keep Alive MSB
+      60, // Keep Alive LSB
+      0,  // Client ID MSB
+      0,  // Client ID LSB
+  };
+
+  unsigned char buf[14];
+
+  lwmqtt_options_t opts = lwmqtt_default_options;
+
+  int l = lwmqtt_serialize_connect(buf, 14, &opts, NULL);
 
   EXPECT_ARRAY_EQ(unsigned char, pkt, buf, l);
 }
