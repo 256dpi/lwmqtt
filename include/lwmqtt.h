b6bf78f4b24c3fb563a4bc86fca3b62719c35d0a
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-03-25 13:22:38 +0100

    added note

--- include/lwmqtt.h
+++ include/lwmqtt.h
@@ -276,6 +276,10 @@ lwmqtt_err_t lwmqtt_unsubscribe(lwmqtt_client_t *client, const char *topic_filte
  */
 lwmqtt_err_t lwmqtt_disconnect(lwmqtt_client_t *client, unsigned int timeout);
 
+// TODO: Improve yield timeouts:
+// Calling yield to check if data is available (read one byte) should only block for a short period, while reading the
+// whole packet my block up to the specified timeout.
+
 /**
  * Will yield control to the client and read from the network and keep the connection alive.
  *
