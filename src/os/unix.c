ec299be6b3518a8289d8a3e664751271f40a8122
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-08-01 00:02:37 +0300

    simplify

--- src/os/unix.c
+++ src/os/unix.c
@@ -134,7 +134,7 @@ lwmqtt_err_t lwmqtt_unix_network_read(lwmqtt_client_t *client, void *ref, void *
   }
 
   // read from socket
-  int bytes = (int)recv(n->socket, &buffer[0], (size_t)len, 0);
+  int bytes = (int)recv(n->socket, buffer, (size_t)len, 0);
   if (bytes < 0 && errno != EAGAIN) {
     return LWMQTT_NETWORK_READ_ERROR;
   }
