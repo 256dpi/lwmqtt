1ff880ce49689ab0b2f852a4ba3eab4d2f050ad3
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-08-17 22:25:23 +0200

    fixed varnum tests

--- tests/helpers.cpp
+++ tests/helpers.cpp
@@ -8,7 +8,7 @@ TEST(VarNum1, Valid) {
   unsigned char buf[2];
   memset(buf, 0, 2);
 
-  for (int i = 0; i < 128; i++) {
+  for (int i = 1; i < 128; i++) {
     unsigned char *ptr1 = buf;
     int ret = lwmqtt_write_varnum((void **)&ptr1, 1, i);
 
@@ -27,15 +27,15 @@ TEST(VarNum2, Valid) {
   unsigned char buf[3];
   memset(buf, 0, 3);
 
-  for (int i = 0; i < 128; i++) {
+  for (int i = 1; i < 128; i++) {
     unsigned char *ptr1 = buf;
-    int ret = lwmqtt_write_varnum((void **)&ptr1, 2, 128 + i * 127);
+    int ret = lwmqtt_write_varnum((void **)&ptr1, 2, 128 * i);
 
     unsigned char *ptr2 = buf;
     long num = lwmqtt_read_varnum((void **)&ptr2, 2);
 
     EXPECT_EQ(0, ret);
-    EXPECT_EQ(128 + i * 127, num);
+    EXPECT_EQ(128 * i, num);
     EXPECT_EQ(0, buf[2]);
     EXPECT_EQ(2, ptr1 - buf);
     EXPECT_EQ(2, ptr2 - buf);
@@ -46,15 +46,15 @@ TEST(VarNum3, Valid) {
   unsigned char buf[4];
   memset(buf, 0, 4);
 
-  for (int i = 0; i < 128; i++) {
+  for (int i = 1; i < 128; i++) {
     unsigned char *ptr1 = buf;
-    int ret = lwmqtt_write_varnum((void **)&ptr1, 3, 128 * 128 + i * 127);
+    int ret = lwmqtt_write_varnum((void **)&ptr1, 3, 128 * 128 * i);
 
     unsigned char *ptr2 = buf;
     long num = lwmqtt_read_varnum((void **)&ptr2, 3);
 
     EXPECT_EQ(0, ret);
-    EXPECT_EQ(128 * 128 + i * 127, num);
+    EXPECT_EQ(128 * 128 * i, num);
     EXPECT_EQ(0, buf[3]);
     EXPECT_EQ(3, ptr1 - buf);
     EXPECT_EQ(3, ptr2 - buf);
@@ -65,17 +65,26 @@ TEST(VarNum4, Valid) {
   unsigned char buf[5];
   memset(buf, 0, 5);
 
-  for (int i = 0; i < 128; i++) {
+  for (int i = 1; i < 128; i++) {
     unsigned char *ptr1 = buf;
-    int ret = lwmqtt_write_varnum((void **)&ptr1, 4, 128 * 128 * 128 + i * 127);
+    int ret = lwmqtt_write_varnum((void **)&ptr1, 4, 128 * 128 * 128 * i);
 
     unsigned char *ptr2 = buf;
     long num = lwmqtt_read_varnum((void **)&ptr2, 4);
 
     EXPECT_EQ(0, ret);
-    EXPECT_EQ(128 * 128 * 128 + i * 127, num);
+    EXPECT_EQ(128 * 128 * 128 * i, num);
     EXPECT_EQ(0, buf[4]);
     EXPECT_EQ(4, ptr1 - buf);
     EXPECT_EQ(4, ptr2 - buf);
   }
+}
+
+TEST(VarNumMax, Valid) {
+  unsigned char buf[4] = {0xFF, 0xFF, 0xFF, 0x7F};
+
+  unsigned char *ptr = buf;
+  long num = lwmqtt_read_varnum((void **)&ptr, 4);
+
+  EXPECT_EQ(268435455, num);
 }
