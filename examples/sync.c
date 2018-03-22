302f65417cba3e11394566511f2c1d9cfc891304
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-03-22 20:22:46 +0100

    improved example

--- examples/sync.c
+++ examples/sync.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <lwmqtt.h>
 #include <lwmqtt/unix.h>
@@ -19,26 +20,32 @@ static void message_arrived(lwmqtt_client_t *client, void *ref, lwmqtt_string_t 
 }
 
 int main() {
+  // initialize client
   lwmqtt_init(&client, malloc(512), 512, malloc(512), 512);
 
+  // configure client
   lwmqtt_set_network(&client, &network, lwmqtt_unix_network_read, lwmqtt_unix_network_write);
   lwmqtt_set_timers(&client, &timer1, &timer2, lwmqtt_unix_timer_set, lwmqtt_unix_timer_get);
   lwmqtt_set_callback(&client, NULL, message_arrived);
 
+  // configure message time
   lwmqtt_unix_timer_set(&timer3, MESSAGE_TIMEOUT);
 
+  // connect to broker
   lwmqtt_err_t err = lwmqtt_unix_network_connect(&network, "broker.shiftr.io", 1883);
   if (err != LWMQTT_SUCCESS) {
     printf("failed lwmqtt_unix_network_connect: %d\n", err);
     exit(1);
   }
 
+  // prepare options
   lwmqtt_options_t options = lwmqtt_default_options;
   options.client_id = lwmqtt_string("lwmqtt");
   options.username = lwmqtt_string("try");
   options.password = lwmqtt_string("try");
   options.keep_alive = 5;
 
+  // send connect packet
   lwmqtt_return_code_t return_code;
   err = lwmqtt_connect(&client, options, NULL, &return_code, COMMAND_TIMEOUT);
   if (err != LWMQTT_SUCCESS) {
@@ -46,15 +53,19 @@ int main() {
     exit(1);
   }
 
+  // log
   printf("connected!\n");
 
+  // subscribe to topic
   err = lwmqtt_subscribe_one(&client, lwmqtt_string("hello"), LWMQTT_QOS0, COMMAND_TIMEOUT);
   if (err != LWMQTT_SUCCESS) {
     printf("failed lwmqtt_subscribe: %d\n", err);
     exit(1);
   }
 
-  while (true) {
+  // loop forever
+  for (;;) {
+    // check if data is available
     size_t available = 0;
     err = lwmqtt_unix_network_peek(&network, &available);
     if (err != LWMQTT_SUCCESS) {
@@ -62,6 +73,7 @@ int main() {
       exit(1);
     }
 
+    // process data if available
     if (available > 0) {
       err = lwmqtt_yield(&client, available, COMMAND_TIMEOUT);
       if (err != LWMQTT_SUCCESS) {
@@ -70,22 +82,30 @@ int main() {
       }
     }
 
+    // keep connection alive
     err = lwmqtt_keep_alive(&client, COMMAND_TIMEOUT);
     if (err != LWMQTT_SUCCESS) {
       printf("failed lwmqtt_keep_alive: %d\n", err);
       exit(1);
     }
 
+    // check if message is due
     if (lwmqtt_unix_timer_get(&timer3) <= 0) {
+      // prepare message
       lwmqtt_message_t msg = {.qos = LWMQTT_QOS0, .retained = false, .payload = (uint8_t *)("world"), .payload_len = 5};
 
+      // publish message
       err = lwmqtt_publish(&client, lwmqtt_string("hello"), msg, COMMAND_TIMEOUT);
       if (err != LWMQTT_SUCCESS) {
         printf("failed lwmqtt_keep_alive: %d\n", err);
         exit(1);
       }
 
+      // reset timer
       lwmqtt_unix_timer_set(&timer3, MESSAGE_TIMEOUT);
     }
+
+    // sleep for 100ms
+    usleep(100 * 1000);
   }
 }
