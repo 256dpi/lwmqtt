343bccdd3b9892715956f47fdfb86d0d8bcec1c0
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-02-04 18:36:37 +0100

    updated header

--- include/lwmqtt.h
+++ include/lwmqtt.h
@@ -82,6 +82,52 @@ typedef struct {
   { LWMQTT_QOS0, false, NULL, 0 }
 
 /**
+ * The object defining the last will of a client.
+ */
+typedef struct {
+  lwmqtt_string_t topic;
+  lwmqtt_qos_t qos;
+  bool retained;
+  lwmqtt_string_t payload;
+} lwmqtt_will_t;
+
+/**
+ * The default initializer for the will object.
+ */
+#define lwmqtt_default_will \
+  { lwmqtt_default_string, LWMQTT_QOS0, false, lwmqtt_default_string }
+
+/**
+ * The object containing the connection options for a client.
+ */
+typedef struct {
+  lwmqtt_string_t client_id;
+  uint16_t keep_alive;
+  bool clean_session;
+  lwmqtt_string_t username;
+  lwmqtt_string_t password;
+} lwmqtt_options_t;
+
+/**
+ * The default initializer for the options object.
+ */
+#define lwmqtt_default_options \
+  { lwmqtt_default_string, 60, true, lwmqtt_default_string, lwmqtt_default_string }
+
+/**
+ * The available return codes transported by the connack packet.
+ */
+typedef enum {
+  LWMQTT_CONNECTION_ACCEPTED = 0,
+  LWMQTT_UNACCEPTABLE_PROTOCOL = 1,
+  LWMQTT_IDENTIFIER_REJECTED = 2,
+  LWMQTT_SERVER_UNAVAILABLE = 3,
+  LWMQTT_BAD_USERNAME_OR_PASSWORD = 4,
+  LWMQTT_NOT_AUTHORIZED = 5,
+  LWMQTT_UNKNOWN_RETURN_CODE = 6
+} lwmqtt_return_code_t;
+
+/**
  * Additional options for publishing messages.
  */
 typedef struct {
@@ -234,52 +280,6 @@ void lwmqtt_set_callback(lwmqtt_client_t *client, void *ref, lwmqtt_callback_t c
 void lwmqtt_drop_overflow(lwmqtt_client_t *client, bool enabled, uint32_t *counter);
 
 /**
- * The object defining the last will of a client.
- */
-typedef struct {
-  lwmqtt_string_t topic;
-  lwmqtt_qos_t qos;
-  bool retained;
-  lwmqtt_string_t payload;
-} lwmqtt_will_t;
-
-/**
- * The default initializer for the will object.
- */
-#define lwmqtt_default_will \
-  { lwmqtt_default_string, LWMQTT_QOS0, false, lwmqtt_default_string }
-
-/**
- * The object containing the connection options for a client.
- */
-typedef struct {
-  lwmqtt_string_t client_id;
-  uint16_t keep_alive;
-  bool clean_session;
-  lwmqtt_string_t username;
-  lwmqtt_string_t password;
-} lwmqtt_options_t;
-
-/**
- * The default initializer for the options object.
- */
-#define lwmqtt_default_options \
-  { lwmqtt_default_string, 60, true, lwmqtt_default_string, lwmqtt_default_string }
-
-/**
- * The available return codes transported by the connack packet.
- */
-typedef enum {
-  LWMQTT_CONNECTION_ACCEPTED = 0,
-  LWMQTT_UNACCEPTABLE_PROTOCOL = 1,
-  LWMQTT_IDENTIFIER_REJECTED = 2,
-  LWMQTT_SERVER_UNAVAILABLE = 3,
-  LWMQTT_BAD_USERNAME_OR_PASSWORD = 4,
-  LWMQTT_NOT_AUTHORIZED = 5,
-  LWMQTT_UNKNOWN_RETURN_CODE = 6
-} lwmqtt_return_code_t;
-
-/**
  * Will send a connect packet and wait for a connack response and set the return code.
  *
  * The network object must already be connected to the server. An error is returned if the broker rejects the
@@ -387,7 +387,7 @@ lwmqtt_err_t lwmqtt_disconnect(lwmqtt_client_t *client, uint32_t timeout);
  * available to read in order to constrain the yield to only receive packets that are already in-flight.
  *
  * If no availability info is given the yield will return after one packet has been successfully read or the deadline
- * has been reached but no single bytes has been received.
+ * has been reached but no single byte has been received.
  *
  * Note: The message callback might be called with incoming messages as part of this call.
  *
